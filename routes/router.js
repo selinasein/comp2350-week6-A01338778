dc36dfa96dfdd3e77f9b19efa3ebadb8404a02c0
selinasein <neulmisscj@gmail.com>
2023-03-04 02:48:14 -0800

    add post router

--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,26 @@
-const router = require('express').Router();
-const database = include('databaseConnection');
-const dbModel = include('databaseAccessLayer');
+const router = require("express").Router();
+const database = include("databaseConnection");
+const dbModel = include("databaseAccessLayer");
 //const dbModel = include('staticData');
 
-router.get('/', async (req, res) => {
-	console.log("page hit");
-	
-	try {
-		const result = await dbModel.getAllUsers();
-		res.render('index', {allUsers: result});
+router.get("/", async (req, res) => {
+  console.log("page hit");
 
-		//Output the results of the query to the Heroku Logs
-		console.log(result);
-	}
-	catch (err) {
-		res.render('error', {message: 'Error reading from MySQL'});
-		console.log("Error reading from mysql");
-	}
+  try {
+    const result = await dbModel.getAllUsers();
+    res.render("index", { allUsers: result });
+
+    //Output the results of the query to the Heroku Logs
+    console.log(result);
+  } catch (err) {
+    res.render("error", { message: "Error reading from MySQL" });
+    console.log("Error reading from mysql");
+  }
+});
+
+router.post("/addUser", (req, res) => {
+  console.log("form submit");
+  console.log(req.body);
 });
 
 module.exports = router;
