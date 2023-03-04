279cf8098dbe45ffe60fe2ccbc0fe986b5299c85
selinasein <neulmisscj@gmail.com>
2023-03-04 02:39:53 -0800

    connect to database

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,29 +1,27 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: "sql.freedb.tech",
+  user: "freedb_2350_main_sel",
+  password: "Qk28*ry9NTw*w8z",
+  database: "freedb_comp2350-A01338778",
+  multipleStatements: false,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+  host: "localhost",
+  user: "root",
+  password: "MySQLtpdls7^*",
+  database: "database",
+  multipleStatements: false,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
